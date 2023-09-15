b3113b8b3140fd58ee52f7d30a4cede1dffe33d9
Mariusz Klimek <klimcio@gmail.com>
2023-09-15 22:39:23 +0200

    Reacting to player input and changing the variable that holds the direction

--- SnakeGame/Game.gd
+++ SnakeGame/Game.gd
@@ -2,6 +2,10 @@ extends Node
 
 signal beat
 
+enum SnakeDirection {UP, DOWN, LEFT, RIGHT}
+
+static var CurrentDirection
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var timer = get_node("Timer")
@@ -9,7 +13,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("move_right"):
+		CurrentDirection = SnakeDirection.RIGHT
+	if Input.is_action_pressed("move_left"):
+		CurrentDirection = SnakeDirection.LEFT
+	if Input.is_action_pressed("move_down"):
+		CurrentDirection = SnakeDirection.DOWN
+	if Input.is_action_pressed("move_up"):
+		CurrentDirection = SnakeDirection.UP
 
 func _on_timer_beat():
 	beat.emit()
